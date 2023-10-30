80241f769d77b36ed893f49996669d4aff75d2e1
Yevhen Korolov <ekorolev08@gmail.com>
2023-10-30 21:53:05 +0200

    react-hw-01-components-00

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,6 +26,7 @@ import friends from '../friends.json';
 
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import transactions from '../transactions.json';
+import { GlobalStyle } from './GLobalStyle';
 
 export const App = () => {
   return (
@@ -37,7 +38,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      
+      < GlobalStyle />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />;
---
+++ src/components/GLobalStyle.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+export const GlobalStyle = createGlobalStyle`
+
+*{
+  box-sizing: border-box;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin: 0;
+  padding: 0;
+}
+
+ul,
+ol {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
+
+`;
