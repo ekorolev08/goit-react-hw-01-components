dff71433b650be7ed08e638b3fe5397f5a35e311
Yevhen Korolov <ekorolev08@gmail.com>
2023-10-30 20:36:47 +0200

    react-hw-01-components-00

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,14 +4,25 @@ import PropTypes from 'prop-types';
 import css from './Profile.module.css';
 
 // const PageTitle = styled.h1`
-// border-color: orange;
+// background-color: ${props => {
+// switch (props.$variant) {
+//   case 'primary':
+//     return 'orange';
+//     case 'secondary':
+//       return 'green';
+//       default:
+//         return 'blue';
+// }
+// }};
 // `;
 
 export const Profile = ({ username, tag, location, avatar, stats }) => {
   return (
     <div className={css.profile}>
       <div className={css.description}>
-        {/* <PageTitle> <HiMail size="40"/>TOP RATED </PageTitle> */}
+        {/* <PageTitle $variant="primary"> <HiMail size="40"/>СЛАВА УКРАЇНІ </PageTitle> */}
+        {/* <PageTitle $variant="secondary"> <HiMail size="40"/>ГЕРОЯМ СЛАВА </PageTitle> */}
+        {/* <PageTitle $variant="default"> <HiMail size="40"/>СМЕРТЬ ВОРОГАМ</PageTitle> */}
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
         <p className={css.tag}>@{tag}</p>
